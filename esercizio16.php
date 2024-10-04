a72b810d038e97c500e61cee3787964b99bb1650
Benncio09 <lorebencini13@outlook.com>
2024-10-04 07:54:59 +0200

    finito e funzionante

--- esercizio16.php
+++ esercizio16.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        function stampaTabella($m, $n){ //m=righe n=colonne
+            $tabella="";
+            for ($i=0; $i<$m; $i++){
+                $row="";
+                for ($j=0; $j<$n; $j++){
+                    $row=$row."<td style='border: 1px, solid, black; border-collapse:collapse; padding:10px'> ($i,$j) </td>";
+
+                }
+                $tabella=$tabella."<tr >".$row."</tr>";
+            }
+            $tabella= "<table style=' border-collapse:collapse;' >".$tabella."</table";
+            echo $tabella;
+        }
+
+        stampaTabella(4,10);
+    ?>
 </body>
 </html>
